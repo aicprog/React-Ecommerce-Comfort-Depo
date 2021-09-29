f64a861a5197a09be9ead2ede86703f775eed40a
Aicprog <aicprog@gmail.com>
2021-09-28 23:42:12 -0700

    Added list and grid view to products page

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -2,9 +2,29 @@ import React from 'react'
 import { useFilterContext } from '../context/filter_context'
 import GridView from './GridView'
 import ListView from './ListView'
+import styled from "styled-components";
 
 const ProductList = () => {
-  return <h4>product list</h4>
+  const {filtered_products: products, grid_view} = useFilterContext();
+
+  if(products.length < 1){
+    return <Heading>Sorry, no products matched your search...</Heading>
+  }
+
+  if(grid_view === false){
+    return <ListView products={products} />;
+  }
+
+  return (
+    <GridView products={products}>
+    
+    </GridView>
+  )
 }
 
 export default ProductList
+
+export const Heading = styled.h5`
+  text-transform: none;
+  padding-top: 5%
+`
